37ebf53cd553d6be9bf43ec7558e30b2770fce58
David Herrmann <dh.herrmann@googlemail.com>
2012-06-24 12:20:18 +0200

    vte: set AUTO_WRAP mode on reset for console object
    We reset all modes but forget to set them on the console object, too. The
    AUTO_WRAP mode is the only mode that matters here, so we can ignore the
    other modes.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>

--- src/vte.c
+++ src/vte.c
@@ -302,6 +302,7 @@ void kmscon_vte_reset(struct kmscon_vte *vte)
 	vte->flags |= FLAG_SEND_RECEIVE_MODE;
 	vte->flags |= FLAG_AUTO_WRAP_MODE;
 	kmscon_console_reset_flags(vte->con, ~0);
+	kmscon_console_set_flags(vte->con, KMSCON_CONSOLE_AUTO_WRAP);
 
 	kmscon_utf8_mach_reset(vte->mach);
 	vte->state = STATE_GROUND;
