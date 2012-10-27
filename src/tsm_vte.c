a939a9e59f0307ed965d3daa0ca067e95e859fc5
Ran Benita <ran234@gmail.com>
2012-10-27 11:50:52 +0200

    tsm_vte: don't send sequence for Scroll_Lock
    This value should not be sent to the application; currently pressing
    Scroll Lock does some weird character transposing, which I believe to be
    completely accidental.
    
    Signed-off-by: Ran Benita <ran234@gmail.com>
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>

--- src/tsm_vte.c
+++ src/tsm_vte.c
@@ -2401,10 +2401,15 @@ bool tsm_vte_handle_keyboard(struct tsm_vte *vte, uint32_t keysym,
 		case XKB_KEY_Pause:
 			vte_write(vte, "\x13", 1);
 			return true;
+		/*
+		 TODO: What should we do on scroll-lock? Sending 0x14 is what
+		       the specs say but it is not used today the way most
+		       users would expect so we disable it. If someone wants
+		       this, we can re-enable it and set some flag.
 		case XKB_KEY_Scroll_Lock:
-			/* TODO: do we need scroll lock impl.? */
 			vte_write(vte, "\x14", 1);
 			return true;
+		*/
 		case XKB_KEY_Sys_Req:
 			vte_write(vte, "\x15", 1);
 			return true;
