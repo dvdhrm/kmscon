0dc3bf80129800eb0a3713df9277f1d850278afc
David Herrmann <dh.herrmann@googlemail.com>
2013-01-07 13:37:17 +0100

    uterm: video: remove drm_get_id() helper
    This helper is no longer used so remove it.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>

--- src/uterm_video_internal.h
+++ src/uterm_video_internal.h
@@ -415,31 +415,6 @@ static inline int video_do_use(struct uterm_video *video)
 
 #if defined(BUILD_ENABLE_VIDEO_DRM) || defined(BUILD_ENABLE_VIDEO_DUMB)
 
-static inline char *video_drm_get_id(int fd)
-{
-	drmSetVersion v = {
-		.drm_di_major = 1,
-		.drm_di_minor = 4,
-		.drm_dd_major = -1,
-		.drm_dd_minor = -1,
-	};
-
-	if (fd < 0)
-		return NULL;
-
-	drmSetMaster(fd);
-	drmSetInterfaceVersion(fd, &v);
-	return drmGetBusid(fd);
-}
-
-static inline void video_drm_free_id(char *id)
-{
-	if (!id)
-		return;
-
-	drmFreeBusid(id);
-}
-
 static inline char *video_drm_get_name(int fd)
 {
 	drmVersionPtr v;
@@ -476,15 +451,6 @@ static inline bool video_drm_available(void)
 }
 
 #else
-
-static inline char *video_drm_get_id(int fd)
-{
-	return NULL;
-}
-
-static inline void video_drm_free_id(char *id)
-{
-}
 
 static inline char *video_drm_get_name(int fd)
 {
