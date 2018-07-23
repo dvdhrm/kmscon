a861a033bf8c695d9ac498b60849c366c60e1895
Peifeng Yu <peifeng@umich.edu>
2018-07-23 15:17:16 -0400

    Update autogen.sh to support out-of-source build

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
-mkdir -p m4
-autoreconf -i
 
-if test ! "x$NOCONFIGURE" = "x1" ; then
-    exec ./configure "$@"
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+origdir=`pwd`
+cd $srcdir
+
+mkdir -p m4
+autoreconf -is --force
+
+cd $origdir
+
+if test -z "$NOCONFIGURE" ; then
+    exec $srcdir/configure "$@"
 fi
